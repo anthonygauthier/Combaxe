315a5dd9ce9826d33e8b3aa17edd33484d34cb56
mrabdibdi <tommyskater@msn.com>
2014-10-09 14:20:31 -0400

    suppression caracteristiqueEquipementsModele ...
    - création insertionCaractéristiquesArmes.sql -
    - Suppression InsertionCaractéristiquesEquipementsModeles.sql -

---
+++ SQL/insertionCaracteristiquesArmes.sql
@@ -0,0 +1,366 @@
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton de pratique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier de pratique')
+,	1
+);
+
+/* FIN PRATIQUE */
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée rouillée')
+,	6
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée rouillée')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton rouillé')
+,	6
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton rouillé')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau rouillé')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau rouillé')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier rouillé')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier rouillé')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Dague rouillée')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Dague rouillée')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Baguette rouillée')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Baguette rouillée')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Hache rouillée')
+,	6
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Hache rouillée')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Lance rouillée')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Lance rouillée')
+,	6
+);
+
+/* FIN ROUILLÉ */
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée magique')
+,	8
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton magique')
+,	8
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau magique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier magique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Dague magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Dague magique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énegie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Baguette magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Baguette magique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Hache magique')
+,	4
+);
+
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Hache magique')
+,	8
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Lance magique')
+,	4
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Lance magique')
+,	8
+);
+
+/* FIN MAGIQUE */
