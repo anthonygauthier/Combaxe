a21a1e7ba59352a89b4f8794daf9ec9a99e9c1ff
mrabdibdi <tommyskater@msn.com>
2014-10-09 10:30:44 -0400

    un autre test

--- SQL/InsertionCaracteristiquesEquipementsModeles.sql
+++ SQL/InsertionCaracteristiquesEquipementsModeles.sql
@@ -10,8 +10,6 @@ VALUES
 =======
 (		(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
 	,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
-	,	7.07
-	,	2
 	,	4
 	);
 >>>>>>> origin/master
