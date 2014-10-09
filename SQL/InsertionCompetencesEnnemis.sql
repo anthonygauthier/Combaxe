6d49a70a2bfcc709395601ccb75b6e1da885292b
mrabdibdi <tommyskater@msn.com>
2014-10-09 15:59:49 -0400

    lien compétence ennemis

--- SQL/InsertionCompetencesEnnemis.sql
+++ SQL/InsertionCompetencesEnnemis.sql
@@ -216,7 +216,7 @@ INSERT INTO CompetencesEnnemis
 VALUES
 (
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Ange déchu')
-,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+,	(SELECT idCompetence FROM Competences WHERE nom='Gardien')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -231,6 +231,14 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Bandit')
+,	(SELECT idCompetence FROM Competences WHERE nom='Empoisonnement')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Centaure')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
 );
@@ -239,8 +247,32 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Centaure')
+,	(SELECT idCompetence FROM Competences WHERE nom='Tir rapide')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Dragon')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Dragon')
+,	(SELECT idCompetence FROM Competences WHERE nom='Souffle de feu')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Dragon')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup de griffe')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -279,6 +311,14 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Farfadet')
+,	(SELECT idCompetence FROM Competences WHERE nom='Mouvement agile')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Gorgone')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
 );
@@ -287,8 +327,24 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Gorgone')
+,	(SELECT idCompetence FROM Competences WHERE nom='Pétrification')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Hydre')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Hydre')
+,	(SELECT idCompetence FROM Competences WHERE nom='Régénération')
 );
 /* FIN COMPÉTENCES ENNEMI A-H*/
 
@@ -305,8 +361,24 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Iguane démoniaque')
+,	(SELECT idCompetence FROM Competences WHERE nom='Invisibilité')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Jaguar géant')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Jaguar géant')
+,	(SELECT idCompetence FROM Competences WHERE nom='Attaque rapide')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -321,8 +393,25 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Kraken')
+,	(SELECT idCompetence FROM Competences WHERE nom='Rafale de tentacules')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Licorne')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Licorne')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup de corne')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -337,8 +426,24 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Minotaure')
+,	(SELECT idCompetence FROM Competences WHERE nom='Furie animale')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Nécromancien')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Nécromancien')
+,	(SELECT idCompetence FROM Competences WHERE nom='Invocation des morts')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -353,10 +458,33 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
-	(SELECT idEnnemi FROM Ennemis WHERE nom='Panthère d\'eau')
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ogre')
+,	(SELECT idCompetence FROM Competences WHERE nom='Tremblement de terre')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ptérodactyle')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
 );
 
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ptérodactyle')
+,	(SELECT idCompetence FROM Competences WHERE nom='Cri strident')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ptérodactyle')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup ailé')
+);
 /* FIN COMPÉTENCES ENNEMI I-P*/
 
 /* DÉBUT COMPÉTENCES ENNEMI Q-Z*/
@@ -372,6 +500,22 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Rhinocéros sauvage')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup de corne')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Rhinocéros sauvage')
+,	(SELECT idCompetence FROM Competences WHERE nom='Piétinement')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Satyre')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
 );
@@ -380,8 +524,25 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Satyre')
+,	(SELECT idCompetence FROM Competences WHERE nom='Mélodie enchantée')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Troll')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Troll')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup foudroyant')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -396,8 +557,40 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ubuntu')
+,	(SELECT idCompetence FROM Competences WHERE nom='Confusion')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Ubuntu')
+,	(SELECT idCompetence FROM Competences WHERE nom='Instabilité')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Vampire')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Vampire')
+,	(SELECT idCompetence FROM Competences WHERE nom='Morsure')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Vampire')
+,	(SELECT idCompetence FROM Competences WHERE nom='Mouvement agile')
 );
 
 INSERT INTO CompetencesEnnemis
@@ -412,6 +605,14 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Worg')
+,	(SELECT idCompetence FROM Competences WHERE nom='Morsure endiablée')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Yéti')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
 );
@@ -420,7 +621,31 @@ INSERT INTO CompetencesEnnemis
 (idEnnemi, idCompetence)
 VALUES
 (
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Yéti')
+,	(SELECT idCompetence FROM Competences WHERE nom='Coup féroce')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
 	(SELECT idEnnemi FROM Ennemis WHERE nom='Zombie')
 ,	(SELECT idCompetence FROM Competences WHERE nom='Attaque physique')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Zombie')
+,	(SELECT idCompetence FROM Competences WHERE nom='Régénération')
+);
+
+INSERT INTO CompetencesEnnemis
+(idEnnemi, idCompetence)
+VALUES
+(
+	(SELECT idEnnemi FROM Ennemis WHERE nom='Zombie')
+,	(SELECT idCompetence FROM Competences WHERE nom='Empoisonnement')
 );
 /* FIN COMPÉTENCES ENNEMI Q-Z*/
