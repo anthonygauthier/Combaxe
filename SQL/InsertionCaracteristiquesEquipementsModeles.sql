10af660c00922bb268c83341bb80ec1a2d9abb23
mrabdibdi <tommyskater@msn.com>
2014-10-09 11:36:22 -0400

    ajout BD
    insertion caractéristiques et les liens

--- SQL/InsertionCaracteristiquesEquipementsModeles.sql
+++ SQL/InsertionCaracteristiquesEquipementsModeles.sql
@@ -1,15 +1,130 @@
 INSERT INTO CaracteristiquesEquipementsModeles
 (idCaracteristique, idEquipementModele, valeur)
 VALUES
-<<<<<<< HEAD
-(
-	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Force')
-,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Plastron de cuir')
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
 ,	1
 );
-=======
-(		(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
-	,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée de pratique')
-	,	4
-	);
->>>>>>> origin/master
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton de pratique')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier de pratique')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bouclier de pratique')
+,	1
+);
+
+
+ /* FIN PRATIQUE */
+ 
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée rouillée')
+,	6
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Épée rouillée')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton rouillé')
+,	6
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Bâton rouillé')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau rouillé')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(	
+	(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='Marteau rouillé')
+,	3
+);
+
+
