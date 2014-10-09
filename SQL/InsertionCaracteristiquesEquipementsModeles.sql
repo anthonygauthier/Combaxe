84ece7a58a43188fe5fe55d85d8de3cef3ebdb35
leptitab <william_themens@hotmail.ca>
2014-10-09 11:38:01 -0400

    Modification BD - Caracteristiques équipements
    Insertion Caractéristiques Équipements Modèles

--- SQL/InsertionCaracteristiquesEquipementsModeles.sql
+++ SQL/InsertionCaracteristiquesEquipementsModeles.sql
@@ -1,15 +1,215 @@
 INSERT INTO CaracteristiquesEquipementsModeles
 (idCaracteristique, idEquipementModele, valeur)
 VALUES
-<<<<<<< HEAD
 (
 	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Force')
 ,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Plastron de cuir')
 ,	1
 );
-=======
-(		(SELECT idCaracteristique FROM  Caracteristiques WHERE nom='Force')
-	,	(SELECT idEquipementModele FROM EquipementModeles WHERE nom='�p�e de pratique')
-	,	4
-	);
->>>>>>> origin/master
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Plastron de cuir')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Plastron de cuir')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Robe de tissu')
+,	3
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Robe de tissu')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Robe de tissu')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Patalon de cuir')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Patalon de cuir')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Patalon de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Pantalon de tissu')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Pantalon de tissu')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Pantalon de tissu')
+,	2
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Force')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Épaulettes de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Épaulettes de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Épaulettes de tissu')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Épaulettes de tissu')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Casque de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Casque de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Couronne')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Couronne')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Défense')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Bottes de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Bottes de cuir')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Énergie')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Souliers')
+,	1
+);
+
+INSERT INTO CaracteristiquesEquipementsModeles
+(idCaracteristique, idEquipementModele, valeur)
+VALUES
+(
+	(SELECT idCaracteristique FROM Caracteristiques WHERE nom='Vitesse')
+,	(SELECT idEquipementModele FROM EquipementsModeles WHERE nom='Souliers')
+,	1
+);
