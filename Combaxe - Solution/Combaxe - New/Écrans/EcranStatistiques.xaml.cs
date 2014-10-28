20b6b2590422c6bb98b5c5d02e251fe6e36135ff
Anthony Gauthier <antho325@hotmail.com>
2014-10-28 10:54:55 -0400

    Mise à jour - Écran de statistiques
    *Ajout de tous les champs dans l'écran de statistiques
    *Ajout de toutes les propriétés propres à chaque champ (nom, margin,
    etc.)
    *Écran de statistiques terminée, mais non fonctionnelle.

--- Combaxe - Solution/Combaxe - New/Écrans/EcranStatistiques.xaml.cs
+++ Combaxe - Solution/Combaxe - New/Écrans/EcranStatistiques.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Combaxe___New.classes;
 
 namespace Combaxe___New.écrans
 {
@@ -22,6 +23,21 @@ namespace Combaxe___New.écrans
         public EcranStatistiques()
         {
             InitializeComponent();
+            nomPersonnageTitre();
+        }
+
+        //Méthode pour afficher le nom du personnage à côté de statistiques dans le titre (Anthony Gauthier - 28/10/2014)
+        private void nomPersonnageTitre()
+        { 
+            lblTitre.Content = lblTitre.Content + " - " + VarGlobales.Personnage.Nom;
+        }
+
+        //Méthode pour le bouton retour (Anthony Gauthier - 28/10/2014)
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            var MenuPrincipal = new EcranMenuPrincipal();
+            MenuPrincipal.Show();
+            this.Close();
         }
     }
 }
