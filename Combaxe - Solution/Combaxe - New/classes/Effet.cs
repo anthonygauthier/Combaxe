36f6398a1f4c3a6c0f223e2e25edab8ec605f2bc
Tommy Gingras <tommyskater@msn.com>
2014-10-28 10:00:46 -0400

    reparer effet.cs

---
+++ Combaxe - Solution/Combaxe - New/classes/Effet.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combaxe___New.classes
+{
+    public class Effet
+    {
+
+        public Effet()
+        {
+
+        }
+
+    }
+}
